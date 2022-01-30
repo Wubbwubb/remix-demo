b194cbf43a888837ed4914d18565ad5ef0d7eebd
Maximilian Schmitt <maximilian.schmitt@bolindalabs.com>
2022-01-30 22:28:34 +0100

    replace fs with google cloud storage

--- app/post.ts
+++ app/post.ts
@@ -1,8 +1,7 @@
-import path from 'path';
-import fs from 'fs/promises';
 import parseFrontMatter from 'front-matter';
 import invariant from 'tiny-invariant';
 import { marked } from 'marked';
+import { Storage } from '@google-cloud/storage';
 
 export type Post = {
   slug: string;
@@ -19,38 +18,71 @@ type NewPost = {
   markdown: string;
 };
 
-const postsPath = path.join(__dirname, '../../../..', 'posts');
+const storage = new Storage({
+  projectId: process.env.GCP_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY!.replace(/\\n/g, '\n')
+  }
+});
+
+const bucketName = 'wubbwubb-remix-netlify-demo';
+const postsPath = 'posts';
 
 function isValidPostAttributes(attributes: any): attributes is PostMarkdownAttributes {
   return attributes?.title;
 }
 
+function getFileName(slug: string) {
+  return `${postsPath}/${slug}.md`;
+}
+
 export async function getPosts() {
-  const dir = await fs.readdir(postsPath);
+  // Lists files in the bucket
+  const [files] = await storage.bucket(bucketName).getFiles({ prefix: `${postsPath}/`, delimiter: '/' });
+
   return Promise.all(
-    dir.map(async filename => {
-      const file = await fs.readFile(path.join(postsPath, filename));
-      const { attributes } = parseFrontMatter(file.toString());
-      invariant(isValidPostAttributes(attributes), `${filename} has bad meta data!`);
-      return {
-        slug: filename.replace(/\.md$/, ''),
-        title: attributes.title
-      };
-    })
+    files
+      .map(file => {
+        let fileName = file.name;
+        const lastSlash = fileName.lastIndexOf('/');
+
+        if (fileName.endsWith('/')) {
+          return '';
+        }
+
+        if (lastSlash >= 0) {
+          fileName = fileName.substring(lastSlash + 1);
+        }
+
+        if (fileName.length <= 3) {
+          return '';
+        }
+
+        return fileName.substring(0, fileName.length - 3);
+      })
+      .filter(slug => slug.length > 0)
+      .map(async slug => {
+        return getPost(slug);
+      })
   );
 }
 
 export async function getPost(slug: string) {
-  const filepath = path.join(postsPath, slug + '.md');
-  const file = await fs.readFile(filepath);
-  const { attributes, body } = parseFrontMatter(file.toString());
-  invariant(isValidPostAttributes(attributes), `Post ${filepath} is missing attributes`);
+  // Downloads the file into a buffer in memory.
+  const fileName = getFileName(slug);
+  const contents = await storage.bucket(bucketName).file(fileName).download();
+
+  const { attributes, body } = parseFrontMatter(contents.toString());
+  invariant(isValidPostAttributes(attributes), `Post ${fileName} is missing attributes`);
   const html = marked(body);
   return { slug, html, title: attributes.title };
 }
 
 export async function createPost(post: NewPost) {
   const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
-  await fs.writeFile(path.join(postsPath, post.slug + '.md'), md);
+
+  await storage.bucket(bucketName).file(getFileName(post.slug)).save(md);
+
   return getPost(post.slug);
 }
